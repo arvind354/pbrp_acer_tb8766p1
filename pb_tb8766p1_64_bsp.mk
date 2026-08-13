f7e9cc138a888ff18f210ed31f179656d781039b
arvind kumar <arvindkumar19111998@gmail.com>
2026-08-13 17:38:13 +0530

    Create pb_tb8766p1_64_bsp.mk

---
+++ pb_tb8766p1_64_bsp.mk
@@ -0,0 +1,5 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+PRODUCT_DEVICE := tb8766p1_64_bsp
+PRODUCT_NAME := pb_tb8766p1_64_bsp
+PRODUCT_BRAND := Acer
+PRODUCT_MODEL := Acer One 8 T4-82L
